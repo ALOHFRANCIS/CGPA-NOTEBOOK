f4ab4943f2dc10064c4291a4ab181f598e47a617
ALOH FRANCIS <alohfrancis39@gmail.com>
2024-05-29 19:37:18 +0100

    successfully added coding.py

---
+++ coding.py
@@ -0,0 +1,24 @@
+
+# pip freeze > requirements.txt~
+greeting ="hello world"
+line01="***************"
+line02="*             *"
+line03="*  WELCOME!   *"
+des="I am "
+first="Aloh"
+last= "Francis"
+fullname=first +" " +  last
+print(greeting)
+print(line01)
+print(line03)
+print(line01)
+print( des +fullname)
+
+
+
+
+
+
+
+
+
